aa87263f9210c6235d2e3c853c42f1b1adf9da83
Chris Mosetick <chris.mosetick@arcteryx.com>
2019-08-27 08:43:31 -0700

    Add httpie CLI utility to pause-image:ubuntu

--- ubuntu/build-push.sh
+++ ubuntu/build-push.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 docker pull ubuntu:18.04
-docker build --no-cache . -t quay.io/cmosetick/pause-image:ubuntu
-docker push quay.io/cmosetick/pause-image:ubuntu
+docker build --no-cache . -t quay.io/cmosetick/pause-image:ubuntu-v3
+docker push quay.io/cmosetick/pause-image:ubuntu-v3
